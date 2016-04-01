89be30dc18b699a202d70d402f1f784d4f621183
KevinFN04 <kevin.felix.04@gmail.com>
2016-04-01 13:02:58 +0200

    Creado el metodo generateStarWarsName Con Constante

--- NameGenerator.java
+++ NameGenerator.java
@@ -7,9 +7,19 @@
  */
 public class NameGenerator
 {    
+    public static final int VALOR_INICIO = 0;
+    public static final int VALOR_FINAL_APELLIDO1 = 3;
+    public static final int VALOR_FINAL_APELLIDO2 = 2;
+    public static final int VALOR_FINAL_NOMBRE = 3;
+    public static final int VALOR_FINAL_CIUDAD = 3;
+
+    /**
+     * Metodo que crea el nombre que tendrias en Star wars 
+     */
     public void generateStarWarsName(String nombre, String apellido1, String apellido2, String ciudad){
-        String starWarsName = apellido1.substring(0,3) + nombre.substring(0,3).toLowerCase() + " " + 
-                              apellido2.substring(0,2) + ciudad.substring(0,3).toLowerCase();
+        
+        String starWarsName = apellido1.substring(NameGenerator.VALOR_INICIO,NameGenerator.VALOR_FINAL_APELLIDO1) + nombre.substring(NameGenerator.VALOR_INICIO,NameGenerator.VALOR_FINAL_NOMBRE).toLowerCase() + " " + 
+                              apellido2.substring(NameGenerator.VALOR_INICIO,NameGenerator.VALOR_FINAL_APELLIDO2) + ciudad.substring(NameGenerator.VALOR_INICIO,NameGenerator.VALOR_FINAL_CIUDAD).toLowerCase();
         System.out.println(starWarsName);
     }
 }
